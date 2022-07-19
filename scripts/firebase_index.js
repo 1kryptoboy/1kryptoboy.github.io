b18bee16be21344da46f8f3bee3825aed934fe00
kryptoboy <1kryptoboy@gmail.com>
2022-07-19 02:53:04 -0500

    added firebase auth

---
+++ scripts/firebase_index.js
@@ -0,0 +1,39 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+    document.getElementById("login_div_reset_password").style.display = "none";
+    var user = firebase.auth().currentUser;
+    if(user != null){
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome back " + email_id;
+    }
+
+  } else {
+    // No user is signed in.
+    //document.getElementById("user_div").style.display = "block";
+    //document.getElementById("login_div").style.display = "block";
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+  }
+});
+
+function login(){
+  console.log("is login even kicking off")
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+  console.log(userEmail)
+  console.log(userPass)
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error : " + errorMessage);
+    // ...
+  });
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
---
+++ scripts/firebase_initialization.js
@@ -0,0 +1,12 @@
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDvVu_7Z0h-DAcwAxAf-EeUQ1FXAgWjApw",
+        authDomain: "kryptoboy-8fb13.firebaseapp.com",
+        databaseURL: "https://kryptoboy-8fb13-default-rtdb.firebaseio.com",
+        projectId: "kryptoboy-8fb13",
+        storageBucket: "kryptoboy-8fb13.appspot.com",
+        messagingSenderId: "G-CZ7KEGYTQS",
+        appId: "1:496306500645:web:b3e8751428e103759cfbcc",
+        measurementId: "G-CZ7KEGYTQS"
+    };
+    firebase.initializeApp(config);
